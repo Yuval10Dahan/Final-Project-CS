56dca7156f5820203e6114fc7950f388f370c34f
Yuval10Dahan <dahan.yuval@gmail.com>
2025-04-20 18:31:28 +0300

    #25

--- Code/Code - not all relevance/Updated_scripts/Important/detect_sensitive_content_in_images.py
+++ Code/Code - not all relevance/Updated_scripts/Important/detect_sensitive_content_in_images.py
@@ -4,33 +4,37 @@ import torch
 
 
 def run_inference():
-    print("🔧 Load fine-tuned model and base processor")
+    print("Load fine-tuned model and base processor")
 
-    # ✅ Load processor from base model
+    # Load processor from base model
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 
-    # ✅ Load fine-tuned model from checkpoint
+    # Load fine-tuned model from checkpoint
     checkpoint_path = "./blip_sensitive_leakage/checkpoint-54000"
     model = BlipForConditionalGeneration.from_pretrained(checkpoint_path)
 
-    # ✅ Load an image
-    print("🖼️ Loading test image")
+    # Load an image
+    print("Loading test image")
+
+    # Bad example
     # testing_image_path = "./images/Generic_letter/letter_1.png"
+
+    # Good example
     testing_image_path = "./images/test/test.jpg"
     image = Image.open(testing_image_path).convert("RGB")
 
-    # ✅ Move to device
-    print("📦 Moving model to device")
+    # Move to device
+    print("Moving model to device")
     device = "cuda" if torch.cuda.is_available() else "cpu"
     inputs = processor(images=image, return_tensors="pt").to(device)
     model = model.to(device)
 
-    # ✅ Generate caption
-    print("🧠 Generating caption...")
+    # Generate caption
+    print("Generating caption...")
     output = model.generate(**inputs)
     caption = processor.decode(output[0], skip_special_tokens=True)
 
-    print("🔍 Generated Caption:", caption)
+    print("Generated Caption:", caption)
 
 
 if __name__ == "__main__":
